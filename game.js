f8d408557b067ee5da53a98877c9eb378eb06538
malikc21 <malikc543@gmail.com>
2023-10-30 22:45:35 -0500

    created funtion to play a single round

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-function getComputerChoice(userpick){ 
+function getComputerChoice(){ 
 
     let choice = Math.floor(Math.random() * 3) + 1;
     let choice2;
@@ -16,4 +16,24 @@ function getComputerChoice(userpick){
     return choice2;
 }
 
-console.log(getComputerChoice("rock"));
+function playround(playerPick, computerPick){
+
+    let result;
+
+    if (playerPick.toLowerCase() === computerPick) {
+        result = "YOU TIED!"
+    }
+    else if (playerPick.toLowerCase() === "rock" && computerPick === "paper") {
+        result = "You Lose LMAO"
+    }
+    else {
+        result = "You WIN YAY"
+    }
+
+    return result
+}
+
+const playerPick = "rOck";
+const computerPick = getComputerChoice();
+
+console.log(playround(playerPick, computerPick));
