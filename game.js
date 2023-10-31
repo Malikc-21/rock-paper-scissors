82ba4e100108f5a59025b2af43b3c375d409b53a
malikc21 <malikc543@gmail.com>
2023-10-30 22:13:26 -0500

    created function to output a random computer pick

---
+++ game.js
@@ -0,0 +1,19 @@
+
+function getComputerChoice(userpick){ 
+
+    let choice = Math.floor(Math.random() * 3) + 1;
+    let choice2;
+
+    if (choice === 1) {
+       choice2 = "rock"
+    }
+    else if (choice === 2) {
+        choice2 = "paper"
+     }
+     else if (choice === 3) {
+        choice2 = "scissors"
+     }
+    return choice2;
+}
+
+console.log(getComputerChoice("rock"));
